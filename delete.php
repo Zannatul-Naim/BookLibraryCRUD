c1c9ad93038e0bc6202ef9883118feaa205a47f0
Zannatul Naim <zannatulnaim09@gmail.com>
2023-10-12 03:09:12 +0600

    Error fixed

--- delete.php
+++ delete.php
@@ -4,14 +4,20 @@
 
     // fetch data from json
     $data = file_get_contents('books.json');
-    $books = json_decode($data);
+    $books = json_decode($data, true);
 
     // delete the row with the index
     unset($books[$index]);
 
+    $books = array_values($books);
+
+    // echo "<pre>";
+    // print_r($books);
+    // echo "</pre>";
+    // var_dump($books);
     // encode back to json
     $data = json_encode($books, JSON_PRETTY_PRINT);
     file_put_contents('books.json', $data);
 
-    header('location: index.php')
+    header('location: index.php');
 ?>
